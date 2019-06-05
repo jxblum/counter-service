8216e8de4a17447e5ee5de477fa0c640a11d874e
John Blum <jblum@pivotal.io>
2019-06-05 01:01:22 -0700

    Use Look-Aside Caching.

--- counter-service-app/src/main/java/example/app/counters/CounterServiceApplication.java
+++ counter-service-app/src/main/java/example/app/counters/CounterServiceApplication.java
@@ -1,10 +1,14 @@
 package example.app.counters;
 
+import org.apache.geode.cache.client.ClientRegionShortcut;
+
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.data.gemfire.config.annotation.EnableCachingDefinedRegions;
 
 @SpringBootApplication
+@EnableCachingDefinedRegions(clientRegionShortcut = ClientRegionShortcut.LOCAL)
 public class CounterServiceApplication {
 
 	public static void main(String[] args) {
