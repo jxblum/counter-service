afd5d68ecbe900e68602bc6f84ba2304826789e9
John Blum <jblum@pivotal.io>
2019-06-05 00:43:52 -0700

    Add timer to the CounterController.

--- counter-service-app/src/main/java/example/app/counters/controller/CounterController.java
+++ counter-service-app/src/main/java/example/app/counters/controller/CounterController.java
@@ -13,35 +13,43 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class CounterController {
 
-	private static final String HEADER_ONE = "<h1>%s</h1>";
+	private static final String HEADER_ONE = "<h1>%s - %s ms</h1>";
 
 	private final CounterService counterService;
 
 	@GetMapping("/ping")
 	public String ping() {
-		return String.format(HEADER_ONE, "PONG");
+		return String.format(HEADER_ONE, "PONG", "NA");
 	}
 
 	@GetMapping("counters/{name}")
 	public String count(@PathVariable("name") String counterName) {
-		return String.format(HEADER_ONE, this.counterService.getCount(counterName));
+
+		long t0 = System.currentTimeMillis();
+
+		return String.format(HEADER_ONE, this.counterService.getCount(counterName),
+			System.currentTimeMillis() - t0);
 	}
 
 	@GetMapping("counters/{name}/current")
 	public String currentCount(@PathVariable("name") String counterName) {
-		return String.format(HEADER_ONE, this.counterService.getCurrentCount(counterName));
+
+		long t0 = System.currentTimeMillis();
+
+		return String.format(HEADER_ONE, this.counterService.getCurrentCount(counterName),
+			System.currentTimeMillis() - t0);
 	}
 
 	@GetMapping("counters/{name}/reset")
 	public String reset(@PathVariable("name") String counterName) {
 		this.counterService.reset(counterName);
-		return String.format(HEADER_ONE, "0");
+		return String.format(HEADER_ONE, "0", "NA");
 	}
 
 	@GetMapping("counters/{name}/use")
 	public String use(@PathVariable("name") String counterName,
 			@RequestParam(name = "count", required = false, defaultValue = "0") long count) {
 
-		return String.format(HEADER_ONE, this.counterService.use(Counter.of(counterName).with(count)).getCount());
+		return String.format(HEADER_ONE, this.counterService.use(Counter.of(counterName).with(count)).getCount(), "NA");
 	}
 }
